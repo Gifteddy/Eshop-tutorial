e6da1005e05017bc3659b7b7413b8b48b9e44485
Gifteddy <114597670+Gifteddy@users.noreply.github.com>
2024-10-19 14:07:53 +0100

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://vercel.live/link/eshop-tutorial-sigma.vercel.app/?via=team-dashboard-project-entity&p=1/api/v2";
+export const server = "https://eshop-tutorial-nu.vercel.app/api/v2";
 
-export const backend_url = "https://vercel.live/link/eshop-tutorial-sigma.vercel.app/?via=team-dashboard-project-entity&p=1/";
+export const backend_url = "https://eshop-tutorial-nu.vercel.app/";
